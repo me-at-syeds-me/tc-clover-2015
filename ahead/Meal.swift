6e568a60357da933dd3057546389cb19da86b4f0
Syed <syed.haider@gorillalogic.com>
2015-09-19 19:00:11 -0700

    adding meals class

---
+++ ahead/Meal.swift
@@ -0,0 +1,33 @@
+//
+//  Meal.swift
+//  ahead
+//
+//  Created by Syed Haider on 9/19/15.
+//  Copyright © 2015 syed. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var orderbtn: UIButton
+    var notifybtn: UIButton
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, orderbtn: UIButton, notifybtn: UIButton) {
+    }
+    
+    // Initialize stored properties.
+    self.name = name
+    self.photo = photo
+    var orderbtn: UIButton
+    var notifybtn: UIButton
+    
+    
+}
+
