7b932010a91a0d430de8faf11f2dc32593449ccf
Syed <syed.haider@gorillalogic.com>
2015-09-19 18:29:47 -0700

    iOS added

---
+++ ahead/ahead/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  ahead
+//
+//  Created by Syed Haider on 9/19/15.
+//  Copyright © 2015 syed. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
